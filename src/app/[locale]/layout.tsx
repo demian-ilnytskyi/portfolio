4150206d637f8d208e73c7b07103e0306a7c8e9d
DemianKryk <demian.ilnytskyi@codinghouse.biz>
2025-06-14 19:28:47 +0300

    small change

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,13 +1,16 @@
 import type { Metadata } from "next";
 import { getTranslations } from 'next-intl/server';
-import { setPageLocaleAsync } from '@/shared/constants/variables/locale_helper';
+import { setPageLocale } from '@/shared/constants/variables/locale_helper';
 import AnalyticsInitScript from "@/shared/components/analytics_init_script";
 import { GoogleAnalytics } from "@next/third-parties/google";
 import { NextIntlClientProvider } from "next-intl";
 import NavigationBar from "@/shared/components/nav_bar/nav_bar";
-import CookieHook from "@/shared/components/cookie/cookie_hook";
 import { cn } from "@/lib/utils";
 import metadataHelper, { openGraph } from "@/shared/helpers/metadata_helper";
+import DeletectThemeScript from "@/shared/components/deletect_theme_script";
+import { cookies } from "next/headers";
+import KTextConstants from "@/shared/constants/variables/text_constants";
+import CookieKey from "@/shared/constants/variables/cookie_key";
 
 
 export async function generateMetadata({ params }: {
@@ -34,28 +37,33 @@ export async function generateMetadata({ params }: {
 
 export default async function RootLayout({
   children,
-  params,
 }: Readonly<{
   children: React.ReactNode;
-  params: Promise<{ locale: Language }>;
 }>): Promise<Component> {
-  const locale = await setPageLocaleAsync(params);
+  const cookie = await cookies();
+  const isDarkMode = cookie.get(CookieKey.isDarkCookieKey)?.value;
+  const locale = (cookie.get(CookieKey.localeCookieName)?.value as Language) ?? KTextConstants.defaultLocale;
+  await setPageLocale({ locale });
+  const htmlClass = (isDarkMode === 'true' && { className: 'dark' });
+
   // const nonce = (await headers()).get('x-nonce') ?? undefined;
-  return <html lang={locale} >
+  return <html lang={locale} {...htmlClass} >
     <head>
       <meta httpEquiv="Content-Language" content={locale} />
       <AnalyticsInitScript />
       {/* Google tag */}
       {process.env.NODE_ENV !== "development"
         && <GoogleAnalytics
-          gaId="G-5K1QYSG097"
+          gaId=""
         // nonce={nonce} 
         />}
+      {!isDarkMode && <DeletectThemeScript />}
     </head>
     <body
-      className={cn(`text-secondary bg-white`)}>
-      <NextIntlClientProvider locale={locale}  >
-        <CookieHook />
+      className={cn(`bg-white dark:bg-gray-900`)}>
+      {/* bg-gray-900 */}
+      <NextIntlClientProvider locale={locale} >
+        {/* <CookieHook /> */}
         <div className="flex flex-col min-h-screen mx-4 desk:mx-24 tablet:mx-8 self-center">
           <NavigationBar />
           {children}
