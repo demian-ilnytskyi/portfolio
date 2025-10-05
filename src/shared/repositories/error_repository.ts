0cee9ddfb7d7c90c6501b869f7bf5b3381f660e5
DemianKryk <demian.ilnytskyi@codinghouse.biz>
2025-10-05 18:53:54 +0300

    improve error handling

--- src/shared/repositories/error_repository.ts
+++ src/shared/repositories/error_repository.ts
@@ -3,10 +3,22 @@
 import Secrets from "../constants/variables/secrets";
 import KTextConstants from "../constants/variables/text_constants";
 
+const originalConsoleError = console.error;
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+console.error = (message?: any, ...optionalParams: any[]) => {
+    sendErrorReport({
+        classOrMethodName: 'Console Error Handler',
+        error: message,
+        optionalParams
+    });
+};
+
 interface ErrorParams {
     error: unknown;
     params?: Record<string, unknown>;
     classOrMethodName: string;
+    optionalParams?: unknown[];
 }
 
 const MAX_TELEGRAM_MESSAGE_LENGTH = 4000;
@@ -20,6 +32,7 @@ export async function sendErrorReport({
     error,
     classOrMethodName,
     params,
+    optionalParams,
 }: ErrorParams): Promise<void> {
     if (!error) {
         return;
@@ -43,6 +56,11 @@ export async function sendErrorReport({
             ? `\n📦 *Params:*\n\`\`\`json\n${JSON.stringify(params, null, 2)}\n\`\`\``
             : "";
 
+        // Prepare optionalParams output if provided
+        const optionalParamsText = optionalParams && optionalParams.length > 0
+            ? `\n🧩 *Optional Params:*\n\`\`\`json\n${JSON.stringify(optionalParams, null, 2)}\n\`\`\``
+            : "";
+
         let text = [
             `🚨 *InflaLite Error Report*`,
             `🕒 *Time:* ${new Date().toLocaleString("uk-UA")}`,
@@ -52,13 +70,14 @@ export async function sendErrorReport({
             `❌ *Error:*`,
             `\`\`\`ts\n${errorText}\n\`\`\``,
             paramsText,
+            optionalParamsText,
         ]
             .filter(Boolean)
             .join("\n");
 
         text = truncateText(text, MAX_TELEGRAM_MESSAGE_LENGTH);
 
-        console.error(text);
+        originalConsoleError(text);
 
         if (!KTextConstants.isDev)
             // Send the HTTP request to Telegram Bot API
