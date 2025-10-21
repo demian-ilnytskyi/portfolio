7bcbc80754187c3d57960c8d1abb7e9adaa02148
DemianKryk <demian.ilnytskyi@codinghouse.biz>
2025-10-21 22:03:32 +0300

    add db queue

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,10 +1,12 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
 import cloudflareCachePurger from "@opennextjs/cloudflare/overrides/cache-purge/index";
+import doQueue from "@opennextjs/cloudflare/overrides/queue/do-queue";
 
 export default defineCloudflareConfig({
   incrementalCache: r2IncrementalCache,
   enableCacheInterception: true,
   routePreloadingBehavior: "none",
   cachePurge: cloudflareCachePurger({ type: 'direct' }),
+  queue: doQueue
 });
