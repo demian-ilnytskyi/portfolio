ce66b85676d393aefe90fcda2a505060c9bac4d3
DemianKryk <demian.ilnytskyi@codinghouse.biz>
2025-10-21 15:24:08 +0300

    global not found use default locale

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,20 +1,17 @@
 import type { Metadata } from "next";
-import { getLocale } from "optimized-next-intl";
 import NotFound, { generateMetadata as notFoundGenerateMetadata } from "./[locale]/not-found";
 import RootLayout from "./[locale]/layout";
 import KTextConstants from "@/shared/constants/variables/text_constants";
 
-export const dynamic = 'force-dynamic';
-
 export async function generateMetadata(): Promise<Metadata | null> {
   if (KTextConstants.isBuild) return null;
-  const locale = await getLocale() as Language;
+  const locale = KTextConstants.defaultLocale;
   return notFoundGenerateMetadata({ params: Promise.resolve({ locale }) });
 };
 
-export default async function GlobalNotFound(): Promise<Component | null> {
+export default function GlobalNotFound(): Component | null {
   if (KTextConstants.isBuild) return null;
-  const locale = await getLocale() as Language;
+  const locale = KTextConstants.defaultLocale;
   return <RootLayout params={Promise.resolve({ locale })}>
     <NotFound params={Promise.resolve({ locale })} />
   </RootLayout>
